9534cc82f5033e5f26958760a7ccbdbffec9f454
Ian Garcia <ian667217@gmail.com>
2026-06-28 21:12:50 -0600

    Added 'rust_gpiozero' and 'ctrlc' crates as dependecies to allow us to interface with the raspberry pi and also interrupt our program with a CTRL-C command

---
+++ backend/target/debug/deps/ring_a_ding_ding-5983da27e3aa41cb.d
@@ -0,0 +1,9 @@
+/Users/ian/Desktop/Development/rust/ring-a-ding-ding/backend/target/debug/deps/ring_a_ding_ding-5983da27e3aa41cb.d: src/main.rs Cargo.toml
+
+/Users/ian/Desktop/Development/rust/ring-a-ding-ding/backend/target/debug/deps/libring_a_ding_ding-5983da27e3aa41cb.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
